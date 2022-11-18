fdd61d79475567076d292f374d1bcc2be9780921
Yami Palavecino <yamiapalavecino@gmail.com>
2022-11-18 16:18:32 -0300

    asyncio

--- tp12/server.py
+++ tp12/server.py
@@ -1,54 +1,48 @@
-import socketserver, subprocess, getopt, sys
+import asyncio, subprocess, getopt, sys
 
 try:
-    opt,arg = getopt.getopt(sys.argv[1:], 'h:p:a:')
-    if len(opt) != 3:
+    opt, arg = getopt.getopt(sys.argv[1:], 'h:p:')
+    if len(opt) != 2:
         print("Por favor ingrese bien la cantidad de argumentos")
         exit()
 except getopt.GetoptError as error:
-    print(f'Error: {error}')
+    print(f'Ha habido un error: {error}')
     exit()
 
-for (op,ar) in opt:
+for (op, ar) in opt:
     if op == '-h':
-       host = str(ar)
+        host = str(ar)
     if op == '-p':
         port = int(ar)
-    if op == '-a':
-        args = str(ar)
 
-class Thread(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
+async def handle_echo(reader, writer):
+    while True:
+        data = await reader.read(100)
+        data = data.decode('ascii')
+        addr = writer.get_extra_info('peername')
+        if data == "bye":
+            print(f"Cliente desconectado {addr}")
+            writer.close()
+            await writer.wait_closed()
+            return
+        print(f"Recibido {data} de {addr}")
+        command = subprocess.Popen([data], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        stdout, stderr = command.communicate()
+        if command.returncode == 0:
+            ans = "OK \n" + stdout
+            print(f'El comando {data} se ejecuto')
+        else:
+            ans = "ERROR \n" + stderr
+            print(f'El comando {data} no se ejecuto')
+        writer.write(ans.encode('ascii'))
+        await writer.drain()
 
-class Process(socketserver.ForkingMixIn, socketserver.TCPServer):
-    pass
-
-class MyTCPHandler(socketserver.BaseRequestHandler):
-
-    def handle(self):
-        while True:
-            data = self.request.recv(1024).strip()
-            if len(data) == 0 or data == "bye":
-                print(f"Cliente desconectado {self.client_address[0]}")
-                exit(0)
-            command = subprocess.Popen([data], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            stdout, stderr = command.communicate()
-            if command.returncode == 0:
-                ans = "OK \n"+ stdout
-                print(f'El comando {data.decode("ascii")} se ejecuto')
-            else:
-                ans = "ERROR \n"+ stderr
-                print(f'El comando {data.decode("ascii")} no se ejecuto')
-
-            self.request.send(ans.encode('ascii'))
-
-if __name__ == "__main__":
-    socketserver.TCPServer.allow_reuse_address = True
-    if args == "p":
-        server = Process((host, port), MyTCPHandler)
-        print(f"Lanzando servidor con procesos (Port= {port})")
-        server.serve_forever()
-    elif args == "t":
-        server = Thread((host, port), MyTCPHandler)
-        print(f"Lanzando servidor con hilos (Port= {port})")
-        server.serve_forever()
+async def main():
+    server = await asyncio.start_server(
+        handle_echo,
+        host,
+        port
+    )
+    print(f'Lanzando servidor')
+    await server.serve_forever()
+asyncio.run(main())
